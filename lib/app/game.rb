4dc3ceb79ba525fd63705eec1f223c9f9cf12cc4
Paul.Graton <paulgtn@hotmail.fr>
2019-01-24 23:01:45 +0100

    Modification des chemins

--- lib/app/game.rb
+++ lib/app/game.rb
@@ -1,4 +1,4 @@
-$:.unshift File.expand_path("/home/paul/Documents/week3/TicTacToe/lib/app/", __FILE__)
+$:.unshift File.expand_path("../lib/app/", __FILE__)
 require 'player'
 require 'board'
 require 'boardcase'
